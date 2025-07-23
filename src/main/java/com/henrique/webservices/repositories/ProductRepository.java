628b63b9a91382aeecfaf18abb6c9b7d97a81670
Henrique <henriquetozzonarciso@gmail.com>
2025-07-23 16:38:59 -0300

    Product

---
+++ src/main/java/com/henrique/webservices/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.henrique.webservices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.henrique.webservices.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
