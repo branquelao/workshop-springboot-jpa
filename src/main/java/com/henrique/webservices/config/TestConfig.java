87d01ada43e720283d26e19d8817eb9eee9d3334
Henrique <henriquetozzonarciso@gmail.com>
2025-07-29 18:10:09 -0300

    Payment, one-to-one association

--- src/main/java/com/henrique/webservices/config/TestConfig.java
+++ src/main/java/com/henrique/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.henrique.webservices.entities.Category;
 import com.henrique.webservices.entities.Order;
 import com.henrique.webservices.entities.OrderItem;
+import com.henrique.webservices.entities.Payment;
 import com.henrique.webservices.entities.Product;
 import com.henrique.webservices.entities.User;
 import com.henrique.webservices.entities.enums.OrderStatus;
@@ -79,7 +80,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
-	
-	
 }
